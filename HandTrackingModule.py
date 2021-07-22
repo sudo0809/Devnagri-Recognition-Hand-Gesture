0b8388c17f393ba21b014592852d8f568d0a6359
Sugat <sugatdhole@gmail.com>
2021-07-22 16:12:41 +0530

    Second commit

---
+++ HandTrackingModule.py
@@ -0,0 +1,105 @@
+import cv2
+import mediapipe as mp
+import time
+
+tipIds = [4, 8, 12, 16, 20]
+
+
+class handDetector():
+    def __init__(self, mode=False, maxhands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxhands = maxhands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxhands, self.detectionCon,
+                                        self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        # print(results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(
+                        img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True, highlightPoint=[4, 8, 12, 16, 20]):
+
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            # print(len(self.results.multi_hand_landmarks))
+            handNo = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(handNo.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cy, cx])
+                if draw and (id in highlightPoint):
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
+        return self.lmList
+
+    def fingersUp(self):
+
+        righthand = True  # by default
+        lefthand = False
+
+        if (len(self.lmList) != 0):
+            fingers = []
+            if self.lmList[2][2] > self.lmList[18][2]:
+                lefthand = True
+                righthand = False
+
+            for finger_tip in tipIds:
+                thumb = False
+                if finger_tip == 4:
+                    thumb = True
+
+                if thumb and righthand and (self.lmList[4][2] < self.lmList[finger_tip-1][2]):
+                    fingers.append(1)
+                elif thumb and lefthand and (self.lmList[4][2] > self.lmList[finger_tip-1][2]):
+                    fingers.append(1)
+
+                elif not thumb and self.lmList[finger_tip][1] < self.lmList[finger_tip - 2][1]:
+                    fingers.append(1)  # if finger is open
+                else:
+                    fingers.append(0)  # if finger is closed
+
+            return fingers
+
+
+def main():
+    pTime = 0
+    cTime = 0
+
+    cap = cv2.VideoCapture(0)
+    detector = handDetector(detectionCon=0.8)
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        # if len(lmList)!=0:
+        # print(lmList)
+        # print()
+        detector.fingersUp()
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
